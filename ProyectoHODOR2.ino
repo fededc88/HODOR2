cf9795b0857ba71f265b10bcec91e9bba9db7042
Federico Ceccarelli <fededc88@gmail.com>
2021-08-22 01:33:39 -0300

    Rev 0.2.1

--- ProyectoHODOR2.ino
+++ ProyectoHODOR2.ino
@@ -1,15 +1,17 @@
 
-/* 
-|| Proyecto HODOR.
-|| Author: Federico David Ceccarelli
-|| fceccarelli@santafe.gov.ar
- 
+/*
+  || Proyecto HODOR.
+  || Author: Federico David Ceccarelli
+  || fceccarelli@santafe.gov.ar
+
 */
 
-//return true when a 3 digits code is entered. Numbers should be pressed in less than 1 second. 
-bool HayCodigo(char*, int,char);
+//return true when a 3 digits code is entered. Numbers should be pressed in less than 1 second.
+bool ArmarCodigo(char*, int, char);
 void PrintMenu(void);
 bool agregarPin(void);
+bool Validacion(void);
+bool Aceptar(void);
 
 
 
@@ -17,7 +19,8 @@ bool agregarPin(void);
 #include <EEPROM.h>
 
 
-#define Ndigitos 3 //numero de digitos del codigo de acceso. 
+#define Ndigitos 3 //numero de digitos del codigo de acceso.
+#define adrPass 1020 //direccion de la Contraseña Maestra en EEPROM
 
 /*--------------------Inicialización de variables-----------------------*/
 char customKey;
@@ -28,10 +31,10 @@ const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 
 char keys[ROWS][COLS] = {
-{'1','2','3','A'},
-{'4','5','6','B'},
-{'7','8','9','C'},
-{'*','0','#','D'}
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
 
 byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the kpd
@@ -41,7 +44,8 @@ unsigned long loopCount;
 unsigned long startTime;
 String msg;
 char code[Ndigitos], tecla;
-int pos=0;
+int pos = 0;
+char pass[3];
 
 /*--------------------Inicialización de KeyPad-----------------------*/
 
@@ -51,150 +55,267 @@ Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 void setup() {
 
 
-    Serial.begin(9600);
-    loopCount = 0;
-    startTime = millis();
-    msg = "";
+  Serial.begin(9600);
+  loopCount = 0;
+  startTime = millis();
+  msg = "";
+
+ // Serial.println(EEPROM.length());
+
+for(int i=0;i<Ndigitos;i++){
+  EEPROM.get(adrPass+i,pass[i]);  
 }
 
+}
 
 void loop() {
 
-  
-  tecla = kpd.getKey();
-    
 
-  if (tecla){
-    if (HayCodigo(code,Ndigitos,kpd.key[0].kchar)){
-      
-     //   CodeFlag = false;
-    
-         Serial.print("contraseña: ");
-         for(int a=0;a<Ndigitos;a++){
-            Serial.print(code[a]);
+  tecla = kpd.getKey();
+
+
+  if (tecla) {
+    if (ArmarCodigo(code, Ndigitos, kpd.key[0].kchar)) {
+
+      //   CodeFlag = false;
+
+      Serial.print("contraseña: ");
+      for (int a = 0; a < Ndigitos; a++) {
+        Serial.print(code[a]);
+      }
+      Serial.println();
+    }
+
+
+
+  }
+
+  if ((kpd.key[0].kstate == HOLD) && kpd.key[0].kchar == 'C' && Validacion()) {
+
+    PrintMenu();
+    while (kpd.key[0].kchar != 'D') {
+      if ( kpd.getKeys()) {
+        if ( kpd.key[0].stateChanged && kpd.key[0].kstate == PRESSED) {
+          switch (kpd.key[0].kchar) {
+
+            case 'A':
+              agregarPin();
+              PrintMenu();
+              break;
+
+            case 'B':
+              Serial.println("CASE B");
+              break;
+
+            case 'D':
+              break;
+
+            default:
+              Serial.println("\nSaca la mano de ahi antonio!!\n\r");
+              PrintMenu();
+              break;
+          }
+
+        }
+
+      }
+
+
+    }
+  }
+
+
+
+}  // End loop
+bool agregarPin(void) {
+
+
+  bool HayPos = false, HayCodigo = false;
+  char pos[2];
+  int EEaddress=0;
+  int codei = 0;
+  char codec[Ndigitos];
+  char prueba[Ndigitos];
+
+
+  //           kpd.getKeys();
+
+  EEaddress = Posicion()*3;
+  
+
+  Serial.println("Ingrese clave de 3 dígitos:");
+
+  while (!HayCodigo) {
+    tecla = kpd.getKey();
+    if (tecla) {
+      if (ArmarCodigo(codec, Ndigitos, tecla)) {
+        Serial.print("Usted ingreso: ");
+        for (int i = 0; i < Ndigitos; i++) {
+          Serial.print(codec[i]);
         }
         Serial.println();
-     }
-    
-     
-        
-     }
-
-     if((kpd.key[0].kstate==HOLD) && kpd.key[0].kchar == 'C'){
-     PrintMenu();
-       while(kpd.key[0].kchar != 'D'){
-          if( kpd.getKeys()){
-            if ( kpd.key[0].stateChanged && kpd.key[0].kstate == PRESSED){
-                  switch (kpd.key[0].kchar){
-
-                    case 'A':
-                      agregarPin();
-                      PrintMenu();
-                      Serial.println(kpd.key[0].kchar);
-                      Serial.println(kpd.key[0].stateChanged);
-                      break;
-
-                    case 'B':
-                     Serial.println("CASE B");
-                     break;
-
-                    default:
-                      break;
-                  }
-              
-            }
-            
-          }
-                    
+        HayCodigo = true;
       }
+    }
   }
-    
 
-    
-}  // End loop
-bool agregarPin(void){
+  if (EEaddress >= 0 && EEaddress < (EEPROM.length()-Ndigitos)) {
+
+    for(int a=0;a<Ndigitos;a++){
+         EEPROM.update(EEaddress+a,codec[a]);
+    }
+
+
+  Serial.print("El numero Guardado es: ");
+   for(int a=0;a<Ndigitos;a++){
+       EEPROM.get(EEaddress+a,prueba[a]);
+       Serial.print(prueba[a]);
+  }
+  Serial.print("\nEn Address: ");
+  Serial.print(EEaddress);
+  }
   
 
-  bool HayPos=false;
-  char pos[2];
-  int posi=0; 
-  int codei=0;
-  char codec[Ndigitos-1];
-  
 
-//           kpd.getKeys();            
-           Serial.println("Ingrese posición:");                                          
-           
-           while(!HayPos){ 
-                   tecla = kpd.getKey();   
-                    if (tecla){
-                          if (HayCodigo(pos,Ndigitos-1,kpd.key[0].kchar)){
-                             posi = atoi(pos);                      
 
-                        Serial.print("Usted ingreso la posicion: ");  
-                        Serial.println(posi);
-                        HayPos=true;                     
-                     }
-                    }
-           }
-                     
-           Serial.println("Ingrese clave de 3 dígitos:"); 
 
-            while(kpd.key[0].kchar != 'D'){ 
-                   tecla = kpd.getKey();   
-                    if (tecla){
-                          if (HayCodigo(codec,Ndigitos,kpd.key[0].kchar)){
-                                                   
-                        codei=atoi(codec);
-                        Serial.print("Code: ");  
-                        Serial.println(codei);
-                     }
-                    }
-            }
-            
-                        
-                    
-                        
-                        
-                      
-       
-  kpd.getKeys();
   return true;
 }
- 
-bool HayCodigo(char * codep, int Nnumeros,char tecla){
+
+int Posicion(void){
+
+  
+  bool HayPos = false;
+  char pos[2];
+  int posi = 0;
   
 
-      if (millis()-timer > 1000) pos=0;
-      
-    //Serial.println(timer);
- 
-     if (tecla){
-                 
-            if(pos<Nnumeros-1){
-                timer=millis();
-                codep[pos]=tecla;
-                pos++;
-            }
-            else{
-//               Serial.print(pos);
-                codep[pos]=tecla;
-                pos=0;
-//              Serial.print(pos);
-                return true;
-            //  CodeFlag = true;
-            }
-                 
-     } 
+  
+Serial.println("Ingrese posición:");
+
+  while (!HayPos) {
+    tecla = kpd.getKey();
+    if (tecla) {
+      if (ArmarCodigo(pos, Ndigitos-1, kpd.key[0].kchar)) {
+        posi = atoi(pos);
+
+        Serial.print("Usted ingreso la posicion: ");
+        Serial.println(posi);
+        if (Aceptar()) {
+          HayPos = true;
+          return posi;
+        }
+        else  Serial.println("Ingrese posición:");
+
+      }
+    }
+  }
+
+  
+
+  
+}
+
+bool Validacion(void) {
+
+  char code[3];
+  bool salir = false;
+  char tecla;
+  
+  Serial.print("\nIngrese Pass...\n\r");
+
+  while (!salir) {
+    tecla = kpd.getKey();
+    if ( ArmarCodigo(code, Ndigitos, tecla)) {
+      if (code[0] == pass[0] && code[1] == pass[1] && code[2] == pass[2]) {
+        salir = true;
+        return true;
+      }
+      else {
+        Serial.print("\nCodigo Incorrecto\n\rIngrese Pass...\n\r");
+      }
+
+    }
+  }
+
   return false;
 }
 
-void PrintMenu(void){
 
-  String menu = "-----------------------MENU - CONFIGURACION ---------------------------\n\n\r(A)gregar nuevo Acceso\n\r(B)orrar nuevo acceso\n\r(D) SALIR\n\n\r------------------------------------------------------------------\n\r";
-  //String RC = "   /s:`        `-/+-                                             \n\r                                    -y+`-o+.  `:oo+omo`                                             \n\r                                  `os` ./ .oyoo-`.sy.                                               \n\r                                 -y: -/+/.`o+/``+h:                                                 \n\r                                +y.-`o`o `+:+./hoso`   `..``````````                                \n\r                              `ys` +/s://s:::h+`.o/doo+//:::////:-:/oo+.                            \n\r                              `+ys. `:-oo-.+h.  `y:m`                 .s+                           \n\r                                `ho /so::`.N/////:+h`-:///:`            s/                          \n\r                                `m+++++-  +m-      +y/-```:h:           +s                          \n\r                                :m/::-   ++`     `+d`      `/`        `/y.`:o/  .:+.                \n\r                                h/ -:./hy    `/oy+/N-   `-:///+o+///:/dhooo/-moo+-.h`               \n\r                               sh`:sdNNd.  -so//://hmhoo++m:..``    `.N/    .m/-/o++`               \n\r                              -Nhdo+MNo` `sy:h+:No+/--+o:-m.      ```-N- `:.o/s.-m-                 \n\r                             .hNo` :Ns` -h/+h- +h  :dso-:ys`      ```+m`  -//:dh//y/                \n\r                           .oNN+ ./+:  :h-yNho+d.  y/ .my+y.      ```do      `hys+ooy`              \n\r                        `-yNNNd`/my`  :y.dNNNNNNo-/s  `m```      `.:ho`       `.Ns+/s.              \n\r                       :hNNNNNy  `   -y.dNNNNNNNNNhy/.s/       `+yo+..:  `:+++//-`                  \n\r                     :hNNNNNNNN:    `h.yNNNNNNNNm:/d/+:   `:/o+:` /o+ym++/-`                        \n\r                   -hNNNNdNNNNNNs.  o:/NNNNNNNNN/+y`   .//ssymo` -+++:`                             \n\r                 `sNNNhhmNNNNNNNN- .s`dNNNNNNNd-os``/+osyhdhdhdh:-`                                 \n\r                :mNh+omNNNNNNNNNm` o`:NNNNNNNm-od+oosyddhhhddyom`                                   \n\r               :Nh- hNNNNNNNNNNNy `o /:.oNNNN-/m+oydhhhhddh+:/+s.                                   \n\r               +:   .sNNNNNNNNNM/ .+    -+`.o.dmddhdhddyo:+o+-`                                     \n\r                      `+MNNNNNNM. -/`o.    .-sdhhddmh+:/oo:`                                        \n\r                        ydNNNNNM` :/`mm- -y//dhdmds:/os/`                                           \n\r                          :dNNNM. :/ hNosNs:ddds//oo:`                                              \n\r                          -sysmN+ .o oNNNs/mds/oo/`                                                 \n\r                       `+hy/:/dNd` s``oo:+h:/so.                                                    \n\r                     `/dy::odmddNs:oh//ohdmd+`                                                      \n\r                    :hs::/ddhhhmh+++hdhdmm/`                                                        \n\r                  `omo+osmdhddmh+//oysss+`                                                          \n\r                   ..`````   `                            \n\r";
+
+
+bool Aceptar(void) {
+
+  bool salir = false;
+  Serial.println();
+  Serial.println("Es correcto?");
+  Serial.println("(A) aceptar - (B) volver a elegir");
+  Serial.println();
+
+  while (!salir) {
+
+    if ( kpd.getKeys()) {
+      if ( kpd.key[0].stateChanged && kpd.key[0].kstate == PRESSED) {
+        switch (kpd.key[0].kchar) {
+
+          case 'A':
+            salir = true;
+            return true;
+            break;
+
+          case 'B':
+            salir = true;
+            return false;
+            break;
+
+          default:
+            Serial.println("\nSaca la mano de ahi antonio!!\n\r");
+            Serial.println("Es correcto?");
+            Serial.println("(A) aceptar - (B) volver a elegir");
+            break;
+        }
+
+      }
+    }
+
+
+  }
+
+
+}
+bool ArmarCodigo(char * codep, int Nnumeros, char tecla) {
+
+
+  if (millis() - timer > 1000) pos = 0;
+
+  //Serial.println(timer);
+
+  if (tecla) {
+
+    if (pos < Nnumeros - 1) {
+      timer = millis();
+      codep[pos] = tecla;
+      //               Serial.print(codep[pos]);
+      pos++;
+
+    }
+    else {
+      //               Serial.print(pos);
+      codep[pos] = tecla;
+      //               Serial.print(codep[pos]);
+      pos = 0;
+      //              Serial.print(pos);
+      return true;
+      //  CodeFlag = true;
+    }
+
+  }
+  return false;
+}
+
+void PrintMenu(void) {
+
+  String menu = "\n\r-----------------------MENU - CONFIGURACION ---------------------------\n\n\r(A)gregar nuevo Acceso\n\r(B)orrar nuevo acceso\n\r(D) SALIR\n\n\r------------------------------------------------------------------\n\r";
   Serial.print(menu);
-//  Serial.print(RC);
-  
 }
 
